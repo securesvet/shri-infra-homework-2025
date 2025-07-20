911277700e2a2531a5d0f182005367ec3e633b42
Garajnik <lexan1699@gmail.com>
2025-07-20 21:07:25 +0700

    Change name to correct

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Leonid
+            Author: Sviatoslav Murzin
           </p>
         </div>
       </div>
